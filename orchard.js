cc81497306a67e64f689073ce6cae2ef663373f7
Richard Glover <newklear2k@falcon.local>
2022-01-23 21:51:48 +1000

    Layout tidied, optimisations made.

--- orchard.js
+++ orchard.js
@@ -2,6 +2,9 @@ document.addEventListener('click', function (event) {
 
     // Are we on one of our links?
     if (!event.path[1].matches('a.top-link.d-inline-flex')) return;
+
+    // We don't want to bubble up today because there is no content
+    event.preventDefault();
     
     // Log our message
     console.log('Anchor clicked: ' + event.timeStamp);
